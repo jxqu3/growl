5c945d3df40e7cb54f671f089474c8a5dced6fad
CheckM4te <chkmtd@gmail.com>
2023-11-14 20:31:58 +0100

    working cross

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"runtime"
@@ -14,10 +14,15 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type GrowlEnv struct {
+	Name  string
+	Value string
+}
 type GrowlCommand struct {
 	Name        string
 	Description string
 	Command     string
+	Env         []GrowlEnv
 	Shell       string
 	ShellArgs   string
 }
@@ -42,7 +47,7 @@ func runGoCmd(cmdname string, args []string, cfg GrowlYaml) error {
 			return nil
 		}
 	}
-	fmt.Println("Executing: go", cmdname, ".")
+	fmt.Println("Executing: go", cmdname, ".", strings.Join(args, " "))
 	args = append([]string{cmdname, "."}, args...)
 	cmd := exec.Command("go", args...)
 	if err := cmd.Run(); err != nil {
@@ -101,26 +106,46 @@ func runCommand(args []string, cfg GrowlYaml) {
 		}
 	}
 
+	for _, v := range cfgCmd.Env {
+		os.Setenv(v.Name, v.Value)
+	}
+
 	cmd := exec.Command(cfgCmd.Shell, cfgCmd.ShellArgs, cfgCmd.Command)
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		log.Fatal(err)
+		printErr(err.Error())
 	}
+}
+
+func initYaml() []byte {
+	y, _ := yaml.Marshal(GrowlYaml{
+		Commands: []GrowlCommand{
+			{
+				Name:        "hello",
+				Description: "Says hello world!",
+				Command:     "echo hello world, %1!",
+			},
+		},
+	})
+	os.WriteFile("growl.yaml", y, 0644)
+	return y
 }
 
 func main() {
 	var cfg GrowlYaml
 	content, err := os.ReadFile("growl.yaml")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Growl.yaml not found. Generating it.")
+		content = initYaml()
 	}
 	yaml.Unmarshal(content, &cfg)
 	app := cli.App{
 		Name:                 "growl",
 		Usage:                "simple go cli tools",
 		EnableBashCompletion: true,
+		SkipFlagParsing:      true,
 		Action: func(c *cli.Context) error {
 			if len(c.Args().Slice()) == 0 {
 				runGoCmd("run", c.Args().Slice(), cfg)
@@ -131,7 +156,8 @@ func main() {
 		},
 		Commands: []*cli.Command{
 			{
-				Name: "run",
+				SkipFlagParsing: true,
+				Name:            "run",
 				Action: func(c *cli.Context) error {
 					runGoCmd("run", c.Args().Slice(), cfg)
 					return nil
@@ -140,6 +166,23 @@ func main() {
 				Aliases: []string{
 					"r",
 				},
+			},
+			{
+				SkipFlagParsing: true,
+				Name:            "init",
+				Action: func(c *cli.Context) error {
+					_, err = os.Stat("growl.yaml")
+					if errors.Is(err, os.ErrNotExist) {
+						initYaml()
+						return nil
+					}
+					if err == nil {
+						return errors.New("growl.yaml already exists")
+					}
+					return nil
+				},
+				Usage:   "Generates growl.yaml",
+				Aliases: []string{},
 			},
 			{
 				Name: "list",
@@ -153,7 +196,85 @@ func main() {
 				},
 			},
 			{
-				Name: "build",
+				Name: "help",
+				Action: func(c *cli.Context) error {
+					if c.Args().Len() > 0 {
+						for _, v := range c.App.Commands {
+							if v.Name == c.Args().Get(0) {
+								fmt.Println(v.UsageText)
+							}
+						}
+						return nil
+					}
+					cli.ShowAppHelp(c)
+					return nil
+				},
+				Usage: "Shows this help",
+				Aliases: []string{
+					"h",
+				},
+			},
+			{
+				Name:      "cross",
+				UsageText: "growl cross --os [os] --arch [arch] --ldflags \"[ldflags]\"\ngrowl cross -o [os] -a [arch] -ld \"[ldflags]\"\nYou can use growl cross list to list available OS and CPU architectures",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "os",
+						Aliases: []string{"o"},
+						Value:   runtime.GOOS,
+					},
+					&cli.StringFlag{
+						Name:    "arch",
+						Aliases: []string{"a"},
+						Value:   runtime.GOARCH,
+					},
+					&cli.StringFlag{
+						Name:    "ldflags",
+						Aliases: []string{"ld"},
+						Value:   "",
+					},
+				},
+				Subcommands: []*cli.Command{
+					{
+						Name:  "list",
+						Usage: "List available OS and CPU architectures",
+						Action: func(c *cli.Context) error {
+							color.Green("Available OS:")
+							for _, v := range knownOS {
+								fmt.Println(v)
+							}
+							color.Green("Available CPU architectures:")
+							for _, v := range knownArch {
+								fmt.Println(v)
+							}
+							return nil
+						},
+					},
+				},
+				Action: func(c *cli.Context) error {
+					color.Green("Flags:")
+					red := color.New(color.FgBlue)
+					red.Print("- os ")
+					os.Setenv("GOOS", c.String("os"))
+					fmt.Println(c.String("os"))
+					red.Print("- arch ")
+					os.Setenv("GOARCH", c.String("arch"))
+					fmt.Println(c.String("arch"))
+					red.Print("- ldflags ")
+					fmt.Println(c.String("ldflags"))
+					color.Green("Building...")
+					args := append(c.Args().Slice(), "--ldflags", c.String("ldflags"))
+					runGoCmd("build", args, cfg)
+					return nil
+				},
+				Usage: "Build to target OS and arch. (growl cross --os=linux --arch=amd64)",
+				Aliases: []string{
+					"c",
+				},
+			},
+			{
+				SkipFlagParsing: true,
+				Name:            "build",
 				Action: func(c *cli.Context) error {
 					runGoCmd("build", c.Args().Slice(), cfg)
 					return nil
@@ -166,6 +287,6 @@ func main() {
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		printErr(err.Error())
 	}
 }
--- readme.md
+++ readme.md
@@ -2,19 +2,34 @@
 A simple cli tool to make it easier to build and run your project, and automatize other tasks, by adding custom commands.  
 This is pretty similar to NodeJS package.json commands.
 
+## installation
+```sh
+go install github.com/checkm4ted/growl
+```
+
 ## usage
 First, make a growl.yaml file in your project. There's a example one in this repo.
 ```yaml
 commands:
-  - name: build # build and run commands written here will overwrite default ones :).
-    description: "echo test"
-    command: echo test
+ # build and run commands written here will overwrite default ones so you can set your own :).
+  # - name: build
+  #   description: "echo test"
+  #   command: echo test
   - name: hello
     description: "prints hello, World!" # this description is unused, this is only for documentation and shown only in command list.
-    command: echo "hello world, %1!" # add args with %1, %2, etc.
-    shell: cmd # optional, default is cmd in Windows, bash in Linux. Set this to the shell you want to use.
-    shellargs: /C # optional, default is /C in Windows, none in Linux. Set this to the arg needed to make the shell execute your command.
+    command: echo hello world, %1! # add args with %1, %2, etc.
+    #shell: cmd > optional, default is cmd in Windows, sh in Linux. Set this to the shell you want to use.
+    #shellargs: /C > optional, default is /C in Windows, -c in Linux. Set this to the arg needed to make the shell execute your command.
+    env: # optional, set environment variables here.
+      - name: TEST
+        value: test
 ```
 
 - List commands in growl.yaml using `growl l # or growl list`
-- 
+- Run them with `growl [command] [args]`
+
+- Run/build your project with `growl [b/build/r/run] [args]`
+- Cross compile with  
+`growl cross --os [os] --arch [arch] --ldflags [ldflags]`  
+or `growl cross -o [os] -a [arch] -ld [ldflags]`
+- Print availbable os/architectures with `growl cross list`
